a361871b84536edc86f067839896422a5ec2f291
afosan <afosan@skiff.com>
2024-01-27 05:43:46 +0300

    done

--- src/PingPong.sol
+++ src/PingPong.sol
@@ -1,4 +1,52 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8;
 
-contract PingPong {}
+/**
+ * @title PingPongEvents
+ * @dev events for PingPong.
+ */
+contract PingPongEvents {
+    /// @notice emit for pings
+    event Ping();
+    /// @notice emit for pongs
+    /// @param txHash in response to Ping() emitted by transaction with the hash
+    event Pong(bytes32 txHash);
+}
+
+/**
+ * @title PingPong
+ * @dev one pings, other pongs.
+ */
+contract PingPong is PingPongEvents {
+    /// @notice only address allowed to ping
+    address public pinger;
+
+    /// @notice caller is expected to be the pinger of the contract
+    /// @dev revert reason if `msg.sender` != `pinger`
+    error NotPinger();
+
+    /// @notice ensures caller is the pinger of the contract
+    /// @dev ensures `msg.sender` == `pinger`
+    modifier onlyPinger() {
+        if (msg.sender != pinger) {
+            revert NotPinger();
+        }
+        _;
+    }
+
+    /// @notice pinger is set to caller
+    constructor() {
+        pinger = msg.sender;
+    }
+
+    /// @notice pinger pings
+    function ping() external onlyPinger {
+        emit Ping();
+    }
+
+    /// @notice someone pongs
+    /// @param txHash in response to Ping() emitted by transaction with the hash
+    function pong(bytes32 txHash) external {
+        emit Pong(txHash);
+    }
+}
--- test/PingPong.t.sol
+++ test/PingPong.t.sol
@@ -1,13 +1,51 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8;
 
 import {Test, console} from "forge-std/Test.sol";
-import {PingPong} from "../src/PingPong.sol";
+import {PingPong, PingPongEvents} from "../src/PingPong.sol";
 
-contract PingPongTest is Test {
+contract PingPongTest is Test, PingPongEvents {
     PingPong public pingpong;
+    address alice;
+    address bob;
 
     function setUp() public {
+        alice = makeAddr("alice");
+        bob = makeAddr("bob");
+        vm.deal(alice, 1 ether);
+        vm.deal(bob, 1 ether);
+
+        vm.startPrank(alice);
         pingpong = new PingPong();
+        vm.stopPrank();
+    }
+
+    function test_Deploy() public {
+        assertEq(alice, pingpong.pinger());
+    }
+
+    function test_Ping() public {
+        vm.startPrank(alice);
+        vm.expectEmit(false, false, false, false, address(pingpong));
+        emit Ping();
+
+        pingpong.ping();
+        vm.stopPrank();
+
+        vm.startPrank(bob);
+        vm.expectRevert(abi.encodeWithSelector(PingPong.NotPinger.selector));
+        pingpong.ping();
+        vm.stopPrank();
+    }
+
+    function test_Pong() public {
+        vm.startPrank(bob);
+        bytes32 data = hex"01";
+
+        vm.expectEmit(false, false, false, true, address(pingpong));
+        emit Pong(data);
+
+        pingpong.pong(data);
+        vm.stopPrank();
     }
 }
